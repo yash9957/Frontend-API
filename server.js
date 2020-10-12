1dac0f86fb2bf9fed503d33b980326c4ade8c3de
yash9957 <53232106+yash9957@users.noreply.github.com>
2020-10-12 21:09:55 +0530

    Add files via upload

--- server.js
+++ server.js
@@ -2,9 +2,9 @@ const express = require('express')
 const bodyParser = require('body-parser')
 const bcrypt = require('bcrypt')
 const knex = require('knex')
-//const cors = require('cors')
 const port = 3000
-const salt = bcrypt.genSalt()
+const app = express()
+
 
 const db = knex({
     client:'pg',
@@ -16,40 +16,32 @@ const db = knex({
     }
 })
 
-db.select('*').from('formdata').then(data => {
+/*db.select('*').from('register').then(data => {
     console.log(data)
 })
-
-const app = express()
-
-
-//app.use(cors())
+*/
 app.use(bodyParser.json())
 
 app.get('/', (req, res) => {
     res.send('')
 })
 
-/*app.post('/register', (req, res) => {
-    const {email, rollno, password} = req.body;
-    db('register').insert({
-        email: email,
-        rollno: rollno,
-        password: password,
-    }).then(console.log)
-})*/
-
 app.post('/register', async (req, res) => {
-    const {email, rollno, password} = req.body;
+    const {roll, name, phone, email, password, branch, div } = req.body;
     try {
         const salt = await bcrypt.genSalt()
         const hashedPassword = await bcrypt.hash(password, salt)
-        console.log(salt)
-        console.log(hashedPassword)
+        //console.log(salt)
+        //console.log(hashedPassword)
         db('register').insert({
+            rollno: roll,
+            name: name,
+            phoneno: phone,
             email: email,
-            rollno: rollno,
+            branch: branch,
+            division: div,
             password: hashedPassword,
+            //permissioncode: nextval('permissioncode_seq'),
         }).then(console.log)
         res.status(201).send('Registered')
     }
@@ -58,52 +50,77 @@ app.post('/register', async (req, res) => {
     }
 })
 
-app.post('/', (req, res) => {
-    const {rollno1, rollno2, rollno3, 
-            name1, name2, name3,
-            phone1, phone2, phone3,
-            email1, email2, email3,
-            password1, password2, password3,
-            domain_and_preference1, domain_and_preference2, domain_and_preference3 } = req.body;
-            db.select('password').from('register').where('rollno', 'rollno1').then(data)
-        if (bcrypt.compare(password1, salt, function(err, isMatch) {})) {
-            try{
-                db('formdata')
-                .insert({
-                rollno1: rollno1,
-                rollno2: rollno2,
-                rollno3: rollno3,
-                name1: name1,
-                name2: name2,
-                name3: name3,
-                phone1: phone1,
-                phone2: phone2,
-                phone3: phone3,
-                email1: email1,
-                email2: email2,
-                email3: email3,
-                password1:password1,
-                password2:password2,
-                password3:password3,
-                domain_and_preference1: domain_and_preference1,
-                domain_and_preference2: domain_and_preference2,
-                domain_and_preference3: domain_and_preference3,
-                }).then(console.log)
-                //res.json(db.database)
-                res.status(201).send(Applied)  
-            }    
-            catch{
-                res.status(500).send('Error')
-            }
-        } 
-        else {
-            res.status(500).send('Err')
+app.post("/signin", async (req, res) => {
+    const { email, password } = req.body;
+    db.select("email", "password")
+      .from('register')
+      .where("email", "=", email)
+      .then( data => {
+        const isValid = await bcrypt.compare(password, data.password);
+        if (isValid) {
+          res.send("Success")
+          //.catch((err) => res.status(400).json("unable to get user"));
+        } else {
+          res.status(400).json("wrong credentials");
         }
-})
-
-//bcrypt.compare(password1, g, function(err, isMatch) {})
-
+      })
+      .catch((err) => res.status(400).json("wrong credentials"));
+});
 
 app.listen(port, () => {
-    console.log(`Example app listening on port ${port}!`)
+  console.log(`App listening on port ${port}!`)
 })
+
+/*app.post ('/groupform', (req, res) => {
+  const {roll1, roll2, roll3, permissioncode1, permissioncode2, permissioncode3} = req.body;
+  const text = `SELECT * FROM register WHERE rollno = ${roll1}`
+        app.query(text, (err, res) => {
+          if(err){
+            console.log(err)
+          }
+          else{
+            console.log(res)
+          }
+        })
+  /*if(isvalid1){
+    if(isvalid2){
+      if(isvalid3){
+        db('group').insert({
+          rollno1: roll1,
+          rollno2: roll2,
+          rollno3: roll3,
+          permissioncode1: permissioncode1,
+          permissioncode2: permissioncode2,
+          permissioncode3: permissioncode3,
+        }).then(console.log)
+        res.status(201).send('Form filled successfully!')
+      }
+      res.status(404).send('Wrong Credentials')
+    }
+    res.status(404).send('Wrong Credentials')
+  }
+  */
+
+
+/*app.post("/signin/groupformationform", async (req, res) => {
+    db.select("email", "password")
+      .from("login")
+      .where("email", "=", req.body.email)
+      .then((data) => {
+        const isValid = bcrypt.compareSync(req.body.password, data[0].password);
+        if (isValid) {
+          return db
+            .select("*")
+            .from("users")
+            .where("email", "=", req.body.email)
+            .then((user) => {
+              res.json(user[0]);
+            })
+            .catch((err) => res.status(400).json("unable to get user"));
+        } else {
+          res.status(400).json("wrong credentials");
+        }
+      })
+      .catch((err) => res.status(400).json("wrong credentials"));
+});
+*/
